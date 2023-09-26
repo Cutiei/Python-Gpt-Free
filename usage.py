2bab22d96c9a43d00c79d7aa47238bdf69a4c003
吴逸轩 <i@e.5i.gs>
2023-09-26 16:53:16 +0100

    上传文件至 /

--- usage.py
+++ usage.py
@@ -1,3 +1,19 @@
 from pyfreegpt import Ask
-for i in range(100):
-        print("\n"*5,f"For .{i}",Ask("你可以帮我写代码吗？,使用python编写一个关于{}的程序".format(i),callback=None))
+
+for i in range(1):
+    print(
+        "\n" * 5,
+        f"For .{i}",
+        Ask("你可以帮我写代码吗？,使用python编写一个关于{}的程序".format(i), callback=None),
+    )
+
+
+from pyfreegpt import GPT
+
+g = GPT()
+for i in range(1):
+    print(
+        "\n" * 5,
+        f"For .{i}",
+        g.Ask("你可以帮我写代码吗？,使用python编写一个关于{}的程序".format(i), callback=lambda x: x),
+    )
